73ef485e03d0aa1b7c95e03708a392ea873f48d1
Unknown <jonas.vanoenen@gmail.com>
2018-05-17 19:23:54 +0200

    Naive and pca speed comparison (soort van)

--- lab5/main.m
+++ lab5/main.m
@@ -1,31 +1,116 @@
-%% Load data
-load 'attachments/omni'
+%% Lab 5 - Load data and reshaping images
+load('attachments/omni');
 
-trainingSet = ones(300,16800);
-testSet = ones(250,16800);
+train_set = ones(300,16800);
+test_set = ones(250,16800);
 
 % Parse data to training and test sets
 for i=1:length(images)
     image = images{i}.img;
     if i < 301
-        trainingSet(i,:) = reshape(image,1,16800);
+        train_set(i,:) = reshape(image,1,16800);
     else
-        testSet(i-300,:) = reshape(image,1,16800);
+        test_set(i-300,:) = reshape(image,1,16800);
     end
 end
-%% PCA
 
-% First 25 dimensions
-d = 25;
-
-% Built in matlab version
-% [coeff, score] = pca(trainingSet, 'numComponents', d);
-coeff = PrincipalComponentAnalysis(trainingSet, 25);
-
-% Show first 9 PCA vectors
-for j=1:9
-    subplot(2,5,j)
-    coeff(:,j) = coeff(:,j) / max(coeff(:,j)); % Normalize
-    imshow(reshape(coeff(:,j), 112,150))
+%% PCA Matlab
+% NumComponents set the dimensionality of the coefficients
+[coeff, ~, eigenvalues] = pca(train_set, 'NumComponents', 50);
+for i = 1:size(coeff, 2)
+    coeff(:, i) = coeff(:, i)/max(coeff(:, i));
 end
+
+% Plot 9 PCA vectors as images
+figure;
+for i = 1:9
+    subplot(2,5,i)
+    imshow(reshape(coeff(:, i), 112, 150));
+end
+
+% Plot the first 50 eigenvalues
+figure;
+plot(eigenvalues(1:50));
+title("First 50 eigenvalues")
+
+%% PCA implementation
+figure;
+[coeff, eigenvalues] = PrincipalComponentAnalysis(train_set, 2);
+for i = 1:size(coeff, 2)
+    coeff(:, i) = coeff(:, i)/max(coeff(:, i));
+end
+
+% Plot 9 PCA vectors as images
+figure;
+for i = 1:9
+    subplot(2,5,i)
+    imshow(reshape(coeff(:, i), 112, 150));
+end
+
+% Plot the first 50 eigenvalues
+figure;
+plot(eigenvalues(1:50));
+title("First 50 eigenvalues")
+
+
+%% Naive immage correlation
+tic
+l = length(train_set(:,1));
+image_correlations = [1:l;ones(1,l)]';
+for i=1:l
+    image = train_set(i,:);
+    best_sim = 0;
+    best_match = 0;
+    
+    % Compare image to all other images in set
+    for j=1:l
+        comparisonImage = train_set(j,:);
+        
+        % Check if the images are not the same
+        if j~=i
+            sim = dot(image, comparisonImage); %similarity
+            
+            % Check if the new similarity is the best
+            if sim > best_sim
+                best_match = j;
+                best_sim = sim;
+            end
+        end
+    end
+    
+    image_correlations(i,2) = best_match;
+end
+toc
+
+%% PCA image correlation
+tic
+l = length(train_set(:,1));
+image_correlations = [1:l;ones(1,l)]';
+for i=1:l
+    image = coeff(:,i);
+    best_sim = 0;
+    best_match = 0;
+    
+    % Compare image to all other images in set
+    for j=1:l
+        comparisonImage = train_set(j,:);
+        
+        % Check if the images are not the same
+        if j~=i
+            sim = dot(image, comparisonImage); %similarity
+            
+            % Check if the new similarity is the best
+            if sim > best_sim
+                best_match = j;
+                best_sim = sim;
+            end
+        end
+    end
+    
+    image_correlations(i,2) = best_match;
+end
+toc
+%% Positioning with Nearest Neigbour
+imagestruct = [ images{:}];
+positions = vertcat ( imagestruct . position );
 
